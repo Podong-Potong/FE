f7dd97d8a1763778adbe65a970ff04fb62fcb21a
장수빈 <jangsubin@jangsubin-ui-MacBookAir.local>
2025-01-11 19:02:42 +0900

    feat(#7): Global Style 적용

--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,14 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
+import GlobalStyles from "./styles/globalStyle";
 
 serviceWorkerRegistration.register();
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
 	<React.StrictMode>
+		<GlobalStyles />
 		<App />
 	</React.StrictMode>
 );
