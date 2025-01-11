ee5f88eaada2dda360e44f9dc0bb801c154d427c
wkdtnqls0506 <wkdtnqls0506@naver.com>
2025-01-12 03:31:27 +0900

    feat(#16): 라우터 설정

--- src/router/router.js
+++ src/router/router.js
@@ -3,12 +3,16 @@ import { createBrowserRouter } from "react-router-dom";
 import { NamedLazy } from "../utils/NamedLazy";
 import { Layout } from "../component/common/Layouts/Layouts";
 import { Challenge } from "../pages/Challenge";
+import { Header } from "../component/common/Layouts/Header";
+import { ReactComponent as LeftIcon } from "../assets/icons/ci_chevron-left.svg";
 
 // 동적으로 로드할 페이지
 const Main = NamedLazy(() => import("../pages/Main"), "Main");
-const Calendar = NamedLazy(() => import("../pages/Calendar"), "Calendar");
+const CalendarPage = NamedLazy(() => import("../pages/CalendarPage"), "CalendarPage");
 const Reward = NamedLazy(() => import("../pages/Reward"), "Reward");
 const MyPage = NamedLazy(() => import("../pages/MyPage"), "MyPage");
+const Goal = NamedLazy(() => import("../pages/Goal"), "Goal");
+const CalendarCategory = NamedLazy(() => import("../pages/CalendarCategory"), "CalendarCategory");
 const StartChallenge = NamedLazy(() => import("../pages/StartChallenge"), "StartChallenge");
 
 export const router = createBrowserRouter([
@@ -19,17 +23,43 @@ export const router = createBrowserRouter([
 				path: "/",
 				element: (
 					<Suspense fallback={<div>Loading...</div>}>
+						<Header
+							leftIcon={<div style={{ width: "36px", height: "36px", background: "#D9D9D9" }}></div>}
+						/>
 						<Main />
 					</Suspense>
 				)
 			},
 			{
 				path: "/calendar",
-				element: (
-					<Suspense fallback={<div>Loading...</div>}>
-						<Calendar />
-					</Suspense>
-				)
+				children: [
+					{
+						path: "",
+						element: (
+							<Suspense fallback={<div>Loading...</div>}>
+								<Header leftIcon={<LeftIcon />} />
+								<CalendarPage />
+							</Suspense>
+						)
+					},
+					{
+						path: "goal",
+						element: (
+							<Suspense fallback={<div>Loading...</div>}>
+								<Goal />
+							</Suspense>
+						)
+					},
+					{
+						path: "category",
+						element: (
+							<Suspense fallback={<div>Loading...</div>}>
+								<Header leftIcon={<LeftIcon />} />
+								<CalendarCategory />
+							</Suspense>
+						)
+					}
+				]
 			},
 			{
 				path: "/reward",
@@ -48,6 +78,15 @@ export const router = createBrowserRouter([
 				)
 			},
 			{
+				path: "/challenge",
+				element: (
+					<Suspense fallback={<div>Loading...</div>}>
+						<Header leftIcon={<LeftIcon />} />
+						<Challenge />
+					</Suspense>
+				)
+			},
+			{
 				path: "/startChallenge",
 				element: (
 					<Suspense fallback={<div>Loading...</div>}>
@@ -58,12 +97,3 @@ export const router = createBrowserRouter([
 		]
 	}
 ]);
-
-// {
-// 	path: "calendar",
-// 	element: (
-// 		<Suspense fallback={<div>Loading...</div>}>
-// 			<Calendar />
-// 		</Suspense>
-// 	)
-// }
