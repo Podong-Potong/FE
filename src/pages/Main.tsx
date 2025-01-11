106ca7c0cc81a6c13674a84466ec71e3dd745800
장수빈 <jangsubin@jangsubin-ui-MacBookAir.local>
2025-01-11 19:04:32 +0900

    feat(#7): Main 화면 구성

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,13 +1,26 @@
+import styled from "styled-components";
 import ConsumHistoryBox from "../component/Main/ConsumHistoryBox.tsx/ConsumHistory";
 import Column from "../component/common/Layouts/Column";
+import { NoProgressCard } from "../component/Home/NoProgressCard";
+import { ProgressCard } from "../component/Home/ProgressCard";
 
 export function Main() {
 	return (
 		<>
 			<Column gap={20}>
+				<LogoSample />
+				{/* 조건부 렌더링으로 챌린지가 있을 때와 없을 때 두 가지 상태를 보여줍니다. */}
+				<NoProgressCard />
+				{/* <ProgressCard /> */}
 				<ConsumHistoryBox category="뮤지컬 및 영화" goal={300000} totalSpent={600000} categoryIcon="🎵" />
 				<ConsumHistoryBox category="뮤지컬 및 영화" goal={50000} totalSpent={24000} categoryIcon="🎵" />
 			</Column>
 		</>
 	);
 }
+
+const LogoSample = styled.div`
+	width: 36px;
+	height: 36px;
+	background: #d9d9d9;
+`;
